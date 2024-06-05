02e0d1ce87d3d4a5bcf2e914015fa90fd85437f3
kgarchie <archiethebig@gmail.com>
2024-06-05 08:11:45 +0300

    added support for vercel fs

--- server/utils/icons.ts
+++ server/utils/icons.ts
@@ -1,9 +1,9 @@
 import { readFile } from "node:fs/promises";
-
 export function __readIcon(name: string) {
     if (!name) return null
     if (!name.endsWith(".svg")) name += ".svg"
-    return readFile(`public/icons/${name}`, "utf-8").catch(e => {
+    const path = isVercel ? `/icons/${name}` : `public/icons/${name}`
+    return readFile(path, "utf-8").catch(e => {
         console.error(e)
         return null
     })  
--- server/utils/runtime.ts
+++ server/utils/runtime.ts
@@ -4,5 +4,5 @@ function checkVercel() {
     return !!(process.env.VERCEL || process.env.NOW_REGION);
 }
 
-const isVercel = checkVercel();
+export const isVercel = checkVercel();
 export const isServerless = isEdgeLight || isVercel || isNetlify || isWorkerd;
