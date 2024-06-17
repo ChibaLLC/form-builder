e4bc7d8e19190b467ec24aa4994630b23cebf233
kgarchie <archiethebig@gmail.com>
2024-06-17 17:54:10 +0300

    Tweaked canvas

--- src/runtime/components/Form/Builder/Canvas.vue
+++ src/runtime/components/Form/Builder/Canvas.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { h, render, ref, type PropType, type Ref, resolveComponent, onMounted } from "vue";
+import {h, render, ref, type PropType, type Ref, resolveComponent, onMounted} from "vue";
 import {Field} from '../../../utils/constants'
 import type {
   ButtonElementData,
@@ -8,7 +8,19 @@ import type {
   FormElementData,
   ImageInputElementData,
   InputElementData, RadioElementData, RichTextElementData, SelectElementData, StaticElementData, TextareaElementData
-} from "~/src/runtime/typings";
+} from "../../../typings";
+import {
+  isInput,
+  isFileInput,
+  isImageInput,
+  isButton,
+  isCheckbox,
+  isRadio,
+  isRichText,
+  isSelect,
+  isStatic,
+  isTextarea
+} from "../../../utils/functions.js";
 
 
 const FieldComponent = resolveComponent("FormElementsRenderer")
@@ -225,7 +237,7 @@ class Elements {
     const element = this._elements[id]
     this._elements.splice(id, 1)
     this._elements.splice(newIndex, 0, element)
-    this._elements = this._elements.map((el, index) => ({ ...el, index }))
+    this._elements = this._elements.map((el, index) => ({...el, index}))
     this.renderedElements = []
     this.target.innerHTML = ''
     this.render()
@@ -315,7 +327,7 @@ onMounted(() => {
       </Transition>
       <div class="icon delete">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-          @click="emits('deleteCanvas', index)">
+             @click="emits('deleteCanvas', index)">
           <path
             d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM13.4142 13.9997L15.182 15.7675L13.7678 17.1817L12 15.4139L10.2322 17.1817L8.81802 15.7675L10.5858 13.9997L8.81802 12.232L10.2322 10.8178L12 12.5855L13.7678 10.8178L15.182 12.232L13.4142 13.9997ZM9 4V6H15V4H9Z">
           </path>
