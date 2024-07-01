f2b6fd565637a10b3608983c6e6028719047acb9
kgarchie <archiethebig@gmail.com>
2024-07-01 10:58:19 +0300

    Moved types, added loading indicator

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,7 @@
-import {defineNuxtModule, createResolver, addComponentsDir, addImportsDir, addImportsSources} from '@nuxt/kit'
+import {defineNuxtModule, createResolver, addComponentsDir, addImportsDir} from '@nuxt/kit'
+import type * as Types from './runtime/types'
+
+export type {Types}
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {
@@ -9,15 +12,12 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-form-builder',
     configKey: 'nuxtFormBuilder',
   },
-  // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
-
     addImportsDir(resolver.resolve('./runtime/utils'), {
       prepend: true
     })
-
     addComponentsDir({
       path: resolver.resolve('./runtime/components')
     }).then()
--- src/runtime/components/Form/Viewer.vue
+++ src/runtime/components/Form/Viewer.vue
@@ -50,12 +50,64 @@ function done() {
 }
 </script>
 <template>
-  <div v-for="form in Object.entries(forms || {})" v-if="currentFormIndex < formLength">
-    <FormRenderer :data="form[1]" @submit="formSubmit(+form[0], form[1])" v-if="currentFormIndex === +form[0]"/>
-  </div>
-  <div v-for="store of Object.entries(stores || {})" v-if="storeLength > 0 && currentFormIndex >= formLength">
-    <StoreRenderer :data="store[1]" @submit="storeSubmit(+store[0], store[1])"
-                   v-if="currentStoreIndex === +store[0]"/>
+  <div class="content__holder">
+    <div v-for="form in Object.entries(forms || {})" v-if="currentFormIndex < formLength">
+      <FormRenderer :data="form[1]" @submit="formSubmit(+form[0], form[1])" v-if="currentFormIndex === +form[0]"/>
+    </div>
+    <div v-for="store of Object.entries(stores || {})" v-if="storeLength > 0 && currentFormIndex >= formLength">
+      <StoreRenderer :data="store[1]" @submit="storeSubmit(+store[0], store[1])"
+                     v-if="currentStoreIndex === +store[0]"/>
+    </div>
+    <div class="processing" v-if="currentFormIndex >= formLength && currentStoreIndex >= storeLength">
+      <div class="loading-spinner"></div>
+    </div>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.content__holder{
+  min-height: 10rem;
+  height: fit-content;
+  max-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+
+.loading-spinner {
+  animation-play-state: running;
+  opacity: 1;
+  position: relative;
+  transition: opacity linear 0.1s;
+}
+
+@keyframes spinner {
+  0% {
+    transform: translate3d(-50%, -50%, 0) rotate(0deg);
+  }
+  100% {
+    transform: translate3d(-50%, -50%, 0) rotate(360deg);
+  }
+}
+
+.loading-spinner.-paused {
+  animation-play-state: paused;
+  opacity: 0.2;
+  transition: opacity linear 0.1s;
+}
+
+.loading-spinner::before {
+  animation: 1.5s linear infinite spinner;
+  animation-play-state: inherit;
+  border: solid 3px #dedede;
+  border-bottom-color: #575757;
+  border-radius: 50%;
+  content: "";
+  height: 40px;
+  left: 50%;
+  opacity: inherit;
+  position: absolute;
+  top: 50%;
+  transform: translate3d(-50%, -50%, 0);
+  width: 40px;
+  will-change: transform;
+}
+</style>
