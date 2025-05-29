42250c78ad488e78e5d049511aae63dc6e4727eb
PrimalDjinn <allan.bosire@ifkafin.com>
2025-05-29 19:48:54 +0300

    reinitialised module

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,5 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  nuxtFormBuilder: {},
+  myModule: {},
   devtools: { enabled: true },
-  compatibilityDate: '2024-12-10',
 })
--- src/runtime/utils/symbols.ts
+++
@@ -1,9 +0,0 @@
-import type { InjectionKey } from "vue"
-
-export const formElementDataListKey = Symbol("FormElementDataListKey") as InjectionKey<string>
-export const formElementDataKey = Symbol("FormElementDataKey") as InjectionKey<string>
-export const storeDataKey = Symbol("StoreDataKey") as InjectionKey<string>
-export const editKey = Symbol("FormEditKey") as InjectionKey<string>
-export const disabledKey = Symbol("FormDisabledKey") as InjectionKey<string>
-export const draggedElementKey = Symbol("PickerDraggedElement") as InjectionKey<string>
-export const activePageIndexKey = Symbol("ActivePageIndex")
