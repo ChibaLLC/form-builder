c122fa7fd2e6c5010e8ef41911960d48e76cd579
Kemboi Elvis <kemboielvis22@gmail.com>
2025-08-17 01:55:26 +0300

    feat: improved components

--- packages/builder/src/components/Fields/SelectField.vue
+++ packages/builder/src/components/Fields/SelectField.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { FormField } from "@/types";
 import { ref } from "vue";
+import { Input } from "../ui/input";
 
 const props = defineProps<{
   field: FormField;
@@ -61,7 +62,7 @@ const clearAllOptions = () => {
       class="block text-sm font-medium text-gray-700 mb-2 cursor-pointer"
       @click="isEditingLabel = true"
     >
-      <input
+      <Input
         v-if="isEditingLabel"
         v-model="field.label"
         @blur="exitFromEditing()"
--- packages/builder/src/components/Fields/TextInputField.vue
+++ packages/builder/src/components/Fields/TextInputField.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { FormField } from "@/types";
 import { ref } from "vue";
+import { Input } from "../ui/input";
 
 const props = defineProps<{
   field: FormField;
@@ -47,7 +48,7 @@ const isEditingLabel = ref(false);
       </span>
     </label>
 
-    <input
+    <Input
       type="text"
       :placeholder="field.placeholder"
       class="w-full px-3 py-2 border border-gray-300 rounded-md"
