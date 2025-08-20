2c79f62b7a97f1642b2b77bc78d6535820562a3f
Kemboi Elvis <kemboielvis22@gmail.com>
2025-08-20 04:09:54 +0300

    added Published form

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 // import { FormBuilderInterface } from "@chiballc/form-builder";
 import FormBuilderInterface from "@chiballc/form-builder/components/FormBuilderInterface.vue";
+import { PublishedFormExample } from "@chiballc/form-builder/index";
 
 // import FormBuilderInterface from "../../builder/src/components/FormBuilderInterface.vue";
 </script>
 
 <template>
   <FormBuilderInterface />
+  <PublishedFormExample />
 </template>
 
 <style scoped></style>
