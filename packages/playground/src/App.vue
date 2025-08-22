dd6b5b0b75b6fb81b7c5209f4e6685fe0592b7cd
Kemboi Elvis <kemboielvis22@gmail.com>
2025-08-23 01:26:11 +0300

    feat: updates

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,14 +1,37 @@
 <script setup lang="ts">
 // import { FormBuilderInterface } from "@chiballc/form-builder";
 import FormBuilderInterface from "@chiballc/form-builder/components/FormBuilderInterface.vue";
-import { PublishedFormExample } from "@chiballc/form-builder/index";
-
-// import FormBuilderInterface from "../../builder/src/components/FormBuilderInterface.vue";
+import {
+  PublishedFormExample,
+  PublishedFormRenderer,
+  type FormSchema,
+} from "@chiballc/form-builder/index";
+import type { Store } from "@chiballc/form-builder/types";
+import { ref } from "vue";
+const form = ref<FormSchema | null>(null);
+const stores = ref<Store[]>([]);
+const handlePublished = (formSchema: FormSchema, store: Store[]) => {
+  form.value = formSchema;
+  stores.value = store;
+};
+const handleSubmit = (data: {
+  formData: Record<string, any>;
+  selectedProducts: any[];
+}) => {
+  console.log(data);
+};
 </script>
 
 <template>
-  <FormBuilderInterface />
-  <PublishedFormExample />
+  <FormBuilderInterface v-if="form == null" @submit="handlePublished" />
+  <PublishedFormRenderer
+    v-if="form"
+    :form="form"
+    :stores="stores"
+    :show-stores="stores.length > 0"
+    @submit="handleSubmit"
+  />
+  <!-- <PublishedFormExample /> -->
 </template>
 
 <style scoped></style>
